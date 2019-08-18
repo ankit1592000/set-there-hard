675177e790e6e93fb6db48a559da94fdfa31c0ef
ankit1592000 <KHATRI1592000@gmail.com>
2019-08-18 16:37:57 +0530

    module-4-solution.

--- site/SpeakGoodBye.js
+++ site/SpeakGoodBye.js
@@ -1,3 +1,4 @@
+(function (window) {
 // NOTE! The steps in this file are basically identical to the ones you
 // performed in the SpeakHello.js file.
 
@@ -8,7 +9,6 @@
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-(function (window) {
 var byeSpeaker = {};
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
@@ -18,13 +18,12 @@ var speakWord = "Good Bye";
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 
-	byeSpeaker.speak = function () {
-  console.log(speakWord + " " + bySpeaker.names);
+	byeSpeaker.speak = function (names) {
+  console.log(speakWord + " " + names);
+  return names;
 }
-
-window.byeSpeaker = byeSpeaker;
-
-})();
-
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
+window.byeSpeaker = byeSpeaker;
+
+})(window);
